a005ea6cb89b6ffbb49f6c7928dc5aad082563fb
Pmsaa <3053370470@qq.com>
2023-05-17 14:02:47 +0800

    A new test

--- test.c
+++ test.c
@@ -4,6 +4,9 @@ int main(void)
 {
     printf("Hello world\n");
 
-    printf("I will modify this file");
+    printf("I will modify this file\n");
+
+    // after pull code to gitpub
+    printf("new modified\n");
     return 0;
 }
